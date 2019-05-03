2253f180ebef2c44aacc7cebc47307cf70043c8e
Wangzihao5325 <416990273@qq.com>
2019-05-03 11:27:28 +0700

    1.配置react-navigation以及其依赖react-native-gesture-handler

--- android/app/src/main/java/com/bitmango/MainActivity.java
+++ android/app/src/main/java/com/bitmango/MainActivity.java
@@ -1,6 +1,9 @@
 package com.bitmango;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +14,18 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "bitMango";
+    }
+
+    /**
+     * react-navigation 配置添加方法
+     */
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate(){
+        return new ReactActivityDelegate(this,getMainComponentName()){
+            @Override
+            protected ReactRootView createRootView(){
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
     }
 }
